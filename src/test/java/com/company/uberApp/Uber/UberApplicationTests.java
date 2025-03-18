f9a6e80191593bc45457cad22a0cc87317bd3c35
98001/yash <yashchauhan.gaya@gmail.com>
2025-03-18 13:47:23 +0530

    initial commit

---
+++ src/test/java/com/company/uberApp/Uber/UberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.uberApp.Uber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
