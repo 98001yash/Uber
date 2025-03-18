5565bb6b7d87e8bbec24df91a1596280cc6e4b0d
98001/yash <yashchauhan.gaya@gmail.com>
2025-03-18 14:08:10 +0530

    added the advices and exceptions

---
+++ src/main/java/com/company/uberApp/Uber/exceptions/RuntimeConflictException.java
@@ -0,0 +1,11 @@
+package com.company.uberApp.Uber.exceptions;
+
+public class RuntimeConflictException extends RuntimeException{
+    public RuntimeConflictException() {
+    }
+
+    public RuntimeConflictException(String message) {
+        super(message);
+    }
+}
+
